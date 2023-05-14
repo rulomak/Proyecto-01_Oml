0aafed8294bc27795918df3f59b963118367f4b6
rulomak <raulabelleira@gmail.com>
2023-05-14 18:39:42 -0300

    final

--- README.md
+++ README.md
@@ -1,1 +1,37 @@
-# Proyecto-01_Oml
+# Machine Learning Operations (MLOps)
+Este proyecto consiste en una API construida con FastAPI, que permite consultar información sobre películas. Los datos se obtuvieron a partir de un dataset en formato CSV, al cual se le realizó un ETL básico para extraer algunos datos anidados. A continuación, se realizó un EDA para explorar los datos y se construyó la API con 7 endpoints disponibles:
+
+## Uso
+
+### Interfaz gráfica
+
+La API incluye una interfaz gráfica. Para acceder a ella, abre un navegador y dirígete a: https://recomendaciones-api.onrender.com  donde está alojada la API.  
+Una vez allí, podrás explorar los diferentes endpoints disponibles, así como hacer pruebas y consultas.
+
+### Endpoints
+
+La API cuenta con los siguientes endpoints:
+
+- `películas_mes`: recibe el nombre de un mes y devuelve una lista de películas estrenadas en ese mes.
+- `películas_dia`: recibe el nombre de un día y devuelve una lista de películas estrenadas en ese día.
+- `franquicia`: recibe el nombre de una franquicia y devuelve información sobre la cantidad de películas, ganancias totales y ganancia promedio de la franquicia.
+- `películas_pais`: recibe el nombre de un país y devuelve información sobre la cantidad de películas producidas en ese país.
+- `productoras`: recibe el nombre de una productora y devuelve información sobre las ganancias totales y cantidad de películas producidas por la misma.
+- `retorno`: recibe el nombre de una película y devuelve información sobre inversión, ganancias, retorno y año de estreno.
+- `recomendación`: recibe el nombre de una película y devuelve una lista de 5 películas similares según un modelo de Machine Learning.
+
+Cada endpoint tiene un conjunto específico de parámetros y devuelve una respuesta en formato JSON con la información correspondiente.
+
+## Contribución
+
+Si deseas contribuir al proyecto, por favor sigue estos pasos:
+
+1. Haz un fork del repositorio. :: https://github.com/rulomak/Proyecto-01_Oml
+2. Crea una rama para tus cambios: `git checkout -b mi-rama`
+3. Realiza los cambios necesarios y haz commit: `git commit -am "Descripción de los cambios"`
+4. Realiza un push a la rama: `git push origin mi-rama`
+5. Crea un pull request en GitHub.
+
+## Autor
+
+- Raul Abelleira
---
+++ app/main.py
@@ -0,0 +1,98 @@
+from fastapi import FastAPI
+import pandas as pd
+from starlette.responses import RedirectResponse
+
+# cargo el DataSet
+df_movies = pd.read_csv('movies_etl_api.csv')
+
+
+app = FastAPI()
+
+@app.get('/')
+def raiz():
+    # funcion que muestra el entorno grafico 
+    return RedirectResponse(url="/docs/")
+
+# funciones de consultas
+@app.get('/peliculas_mes/{mes}')
+def peliculas_mes(mes:str):
+    filtered_movies = df_movies[df_movies['mes_es'] == mes.lower()]
+    cantidad_peliculas = len(filtered_movies)
+    return {'mes': mes, 'cantidad': cantidad_peliculas}  
+
+
+@app.get('/peliculas_dia/{dia}')  # dis o dia
+def peliculas_dia(dia:str):
+    filtered_movies = df_movies[df_movies['dia_es'] == dia.lower()]
+    cantidad_peliculas = len(filtered_movies)
+    return {'dia': dia, 'cantidad': cantidad_peliculas}  
+
+
+@app.get('/franquicia/{franquicia}')
+def franquicia(franquicia:str):
+    fran = df_movies[df_movies['name_franquicia'].str.contains(franquicia, case=False)]
+    cantidad = len(fran)
+    ganancia_total = fran['revenue'].sum()
+    ganancia_promedio = ganancia_total / cantidad     
+    
+    return {'franquicia':franquicia, 'cantidad':cantidad, 
+            'ganancia_total':ganancia_total, 
+            'ganancia_promedio': ganancia_promedio}
+
+
+@app.get('/peliculas_pais/{pais}')
+def peliculas_pais(pais:str):
+    pais_df = df_movies[df_movies['country_name'].str.contains(pais, case=False)]
+    cantidad = pais_df.shape[0]
+    return {'pais':pais, 'cantidad':cantidad}   
+
+
+
+@app.get('/productoras/{productora}')
+def productoras(productora:str):
+    filtered_movies = df_movies[df_movies['name_production_compani'].str.contains(productora, case=False)]
+    ganancia_total = filtered_movies['revenue'].sum()
+    cantidad_peliculas = len(filtered_movies)
+    return {'productora': productora, 'ganancia_total': ganancia_total, 'cantidad': cantidad_peliculas}
+    
+
+
+
+@app.get('/retorno/{pelicula}')
+def retorno(pelicula):
+    movie = df_movies[df_movies['title'] == pelicula]
+    inversion = float(movie['budget'].iloc[0])
+    ganancia  = float(movie['revenue'].iloc[0])
+    retorno = float(movie['return'].iloc[0])
+    anio = int(movie['release_year'].iloc[0])        
+    
+    return {'pelicula':pelicula, 'inversion':inversion, 'ganancia':ganancia,'retorno':retorno, 'anio':anio}
+
+
+## Modelo ML  
+
+# librerias 
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.metrics.pairwise import cosine_similarity
+from typing import List, Dict
+
+
+
+@app.get('/recomendacion/{titulo}')
+def recomendacion(titulo: str) -> Dict[str, List[str]]:
+    # Obtengo la fila de la película de entrada
+    movie_row = df_movies.loc[df_movies['title'] == titulo]
+
+    # vector de características de la trama de la película de entrada
+    vectorizer = TfidfVectorizer(stop_words='english')
+    tfidf_matrix = vectorizer.fit_transform(df_movies['overview'].dropna())
+    movie_tfidf = vectorizer.transform(movie_row['overview'])
+
+    # Calcula la similitud del coseno entre la película de entrada y todas las demás películas
+    similarity_scores = cosine_similarity(movie_tfidf, tfidf_matrix)
+
+    # Ordena las películas por su similitud con la película de entrada y devolver las 5 más similares
+    similar_indices = similarity_scores.argsort()[0][-6:-1]
+    recommended_titles = list(df_movies.iloc[similar_indices]['title'].values)
+
+    return {'recomendacion': recommended_titles}
